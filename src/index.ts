23bfb6f4f8609c147426f2e456f25a440898dc36
Andrzej Bartosiński <neob91@gmail.com>
2020-02-04 21:02:03 +0100

    Add extra functions

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,16 @@ export const getNextSunday = (date: string | Date): string => {
   return getDateAsString(newDateObj);
 };
 
+export const getCommerceSundays = (year: number): Date[] => {
+  return commerceSundays
+    .filter(d => d.indexOf(`${year}-`) === 0)
+    .sort()
+    .map(d => new Date(d));
+};
+
 export const isDateSunday = (date: string | Date): boolean => {
   const dateObj = getDateAsObject(date);
   return dateObj.getDay() === 0;
 };
+
+export const isTodaySunday = (): boolean => isDateSunday(new Date());
